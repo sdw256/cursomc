056935c60a51177132516cb6a46f5f0ebb4f51b7
Kleber Santos <sdw256@gmail.com>
2020-02-04 23:49:09 -0300

    Banco de dados H2 e tabela categoria

--- src/main/java/com/kleber/curso/domain/Categoria.java
+++ src/main/java/com/kleber/curso/domain/Categoria.java
@@ -2,11 +2,20 @@ package com.kleber.curso.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable{
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 6184912339184618055L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
